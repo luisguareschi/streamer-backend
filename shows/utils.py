fdd5e99597e71478eecd8db57ba1956a50f6abf1
luisguareschi <luisguareschi@hotmail.com>
2025-03-13 12:18:11 +0100

    Tried adding custom paths for chrome

--- shows/utils.py
+++ shows/utils.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
 import time
 
 def scrape_video_source(url: str) -> str | None:
@@ -9,12 +10,15 @@ def scrape_video_source(url: str) -> str | None:
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     # FOR DIGITAL OCEAN ONLY
+    chrome_path = "/workspace/.chromium/opt/google/chrome/chrome"
+    chromedriver_path = "/workspace/.chromedriver/chromedriver"
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')  # DigitalOcean App Platform requires this
     options.add_argument('--remote-debugging-port=9222')
-    options.binary_location = "/usr/bin/google-chrome"
+    options.binary_location = chrome_path
+    service = Service(chromedriver_path)
     # END OF DIGITAL OCEAN ONLY
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options, service=service)
     driver.get(url)
     timer = 10 # Maximum time to wait for the video tag to load
     video_tag = None
